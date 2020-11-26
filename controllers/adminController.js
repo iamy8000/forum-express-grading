a90fc167837152da976bba88c863fa36dc5077ff
iamy8000 <amy8000@gmail.com>
2020-11-26 15:36:54 +0800

    add admin restaurant page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,6 +7,11 @@ const adminController = {
       return res.render('admin/restaurants', { restaurants })
     })
   },
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
+      return res.render('admin/restaurant', { restaurant })
+    })
+  },
   createRestaurant: (req, res) => {
     return res.render('admin/create')
   },
