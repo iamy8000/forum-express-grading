eeb0f26a8b07ef4d17682b706d605504e9edaed7
iamy8000 <amy8000@gmail.com>
2020-11-26 15:55:04 +0800

    install method-override

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const methodOverride = require('method-override')
 
 const db = require('./models')
 const app = express()
@@ -23,6 +24,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   next()
 })
+app.use(methodOverride('_method'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
