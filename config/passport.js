4c7c0120049394dc3defaef2f666d14d0d34b589
iamy8000 <amy8000@gmail.com>
2020-12-03 23:46:51 +0800

    Add add/remove favorite button to restaurant single page, Switch add/remove favorite button on restaurants index page

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const Restaurant = db.Restaurant
 
 passport.use(new LocalStrategy(
   {
@@ -23,8 +24,12 @@ passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
 passport.deserializeUser((id, cb) => {
-  User.findByPk(id).then(user => {
-    user = user.toJSON() 
+  User.findByPk(id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' }
+    ]
+  }).then(user => {
+    user = user.toJSON()
     return cb(null, user)
   })
 })
--- controllers/restController.js
+++ controllers/restController.js
@@ -35,7 +35,8 @@ const restController = {
       const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
-        categoryName: r.Category.name
+        categoryName: r.Category.name,
+        isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
       }))
       Category.findAll({
         raw: true,
@@ -57,13 +58,16 @@ const restController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
-        { model: Comment, include: [User] }
+        { model: Comment, include: [User] },
+        { model: User, as: 'FavoritedUsers' }
       ]
     }).then(restaurant => {
       return restaurant.increment({ 'viewCounts': 1 })
         .then(restaurant => {
+          const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
           return res.render('restaurant', {
-            restaurant: restaurant.toJSON()
+            restaurant: restaurant.toJSON(),
+            isFavorited: isFavorited
           })
         })
     })
