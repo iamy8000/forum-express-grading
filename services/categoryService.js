4d67328c174ced26cdbee06209f8bd2e45bb216b
iamy8000 <amy8000@gmail.com>
2020-12-08 18:08:23 +0800

    PUT /api/admin/categories/:id

--- services/categoryService.js
+++ services/categoryService.js
@@ -23,15 +23,25 @@ const categoryService = {
   postCategory: (req, res, callback) => {
     if (!req.body.name) {
       return callback({ status: 'error', message: "name didn't exist" })
-      // req.flash('error_messages', "name didn't exist")
-      // return res.redirect('back')
     } else {
       return Category.create({
         name: req.body.name
       })
         .then((category) => {
-          callback({status: 'success', message: ""})
-          // res.redirect('/admin/categories')
+          callback({ status: 'success', message: "" })
+        })
+    }
+  },
+  putCategory: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({ status: 'error', message: "name didn't exist" })
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((category) => {
+              callback({ status: 'success', message: 'category was successfully updated' })
+            })
         })
     }
   }
